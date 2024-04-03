d6129ad1990c17121c47eec3291772f0e9d8684c
jhcp <jhagendo@calpoly.edu>
2024-04-02 17:40:20 -0700

    made the app my own!

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="logo">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="logo">Vite + React</h1>
+      <div className="card logo">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
