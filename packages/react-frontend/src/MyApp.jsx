bf1e015f967c3bb6a6aca108ec8cd3151869c53b
jhcp <jhagendo@calpoly.edu>
2024-04-04 17:37:05 -0700

    Changed name, finished form

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Table from "./Table.jsx";
+import Form from "./Form.jsx";
+
+function MyApp() {
+  const [characters, setCharacters] = useState([]);
+  function removeOneCharacter(index) {
+    const updated = characters.filter((c, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
+}
+
+export default MyApp;
